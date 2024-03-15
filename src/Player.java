a505e9ad3f06fb80054896032f41364668022ad4
BT_1N3_04 <sample@bths.edu>
2024-03-15 08:43:49 -0400

    initial commit

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
 public class Player {
+
+    private String name;
+    private int netWorth;
+    private ArrayList<Property>[] portfolio;
+
+    public Player(String name, int netWorth, ArrayList<Property>[] portfolio) {
+        this.name = name;
+        this.netWorth = netWorth;
+        this.portfolio = portfolio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNetWorth() {
+        return netWorth;
+    }
+
+    public ArrayList<Property>[] getPortfolio() {
+        return portfolio;
+    }
+
+    public void addMoney(int deposit) {
+        netWorth += deposit;
+    }
+
+    public void deductMoney(int money) {
+        if (netWorth >= money) {
+            netWorth -= money;
+        } else {
+            System.out.println("Sorry, you dont have enough funds for that!");
+        }
+    }
+
 
 }
