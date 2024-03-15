45c2facdb65914e9fa3ce4e81b4d84993ee45b0f
BT_1N3_06 <sample@bths.edu>
2024-03-15 09:25:32 -0400

    started Property class

--- src/Property.java
+++ src/Property.java
@@ -1,3 +1,13 @@
 public class Property {
+    private String name;
+    private int cost;
+    private int rent;
+    public Property(String name, int cost, int rent) {
+        this.name = name;
+        this.cost = cost;
+        this.rent = rent;
+    }
+
+
 
 }
