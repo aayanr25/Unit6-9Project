2769c8876586ad6a0da2c9340689ca651157be3a
BT_1N3_06 <sample@bths.edu>
2024-03-15 09:28:23 -0400

    started board

--- src/Property.java
+++ src/Property.java
@@ -1,11 +1,16 @@
+import java.util.ArrayList;
+
 public class Property {
     private String name;
     private int cost;
     private int rent;
+    private ArrayList<Property> allProperties;
     public Property(String name, int cost, int rent) {
         this.name = name;
         this.cost = cost;
         this.rent = rent;
+        initializePropList();
+
     }
 
     public String getName() {
@@ -17,13 +22,35 @@ public class Property {
     public int getRent() {
         return rent;
     }
-    public void buyProperty(Player player) {
+    public boolean buyProperty(Player player) {
         if (player.deductMoney(cost)) {
             player.addToPortfolio(this);
+            return true;
         }
+        return false;
+    }
+    public boolean chargeRent(Player player) {
+        return player.deductMoney(rent);
+    }
+
+    public Property getProperty(String propName) {
+        for (Property prop : allProperties) {
+            if (prop.getName().equals(propName)) {
+                return prop;
+            }
+        }
+        return null;
     }
 
 
+
+
+
+
+    private void initializePropList() {
+        allProperties = new ArrayList<>();
+        allProperties.add(new Property("Boardwalk", 400, 50));
+    }
 
 
 
--- src/Turn.java
+++ src/Turn.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Turn {
     private boolean isGameOver = false;
@@ -6,10 +7,13 @@ public class Turn {
     private int d2;
     private int doubles;
     private Player player;
+    private Scanner scan;
+    private String[][] board;
     private ArrayList<Player> playerList = new ArrayList<>();
 
 
     public Turn() {
+        scan = new Scanner(System.in);
         while (!isGameOver) {
             for (Player player : playerList) {
                 this.player = player;
@@ -20,6 +24,7 @@ public class Turn {
 
     private void playTurn() {
         menu();
+
     }
 
     private int getRoll() {
@@ -36,7 +41,17 @@ public class Turn {
         return d1 == d2;
     }
 
+    private void setupBoard() {
+        board = new String[7][7];
+        for (int i = 1; i < board.length - 1; i++) {
+            for (int j = 1; j < board[0].length - 1; j++) {
+                board[i][j] = "X";
+            }
+        }
+    }
+
     private void menu() {
         System.out.println("It's your turn, " + player.getName());
+
     }
 }
