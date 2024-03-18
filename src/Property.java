974fdce43672c4754d2f960f62ea8c3744414440
BT_1N3_06 <sample@bths.edu>
2024-03-18 09:29:33 -0400

    initialized property list.

--- src/Property.java
+++ src/Property.java
@@ -49,7 +49,28 @@ public class Property {
 
     private void initializePropList() {
         allProperties = new ArrayList<>();
-        allProperties.add(new Property("Boardwalk", 400, 50));
+        allProperties.add(new Property("Grand Central", 600, 50));
+        allProperties.add(new Property("JFK Airport", 600, 50));
+        allProperties.add(new Property("Broadway", 600, 50));
+        allProperties.add(new Property("Brooklyn Tech", 500, 40));
+        allProperties.add(new Property("Brighton Boardwalk", 500, 40));
+        allProperties.add(new Property("Rockaway Beach", 500, 40));
+        allProperties.add(new Property("The Met", 450, 35));
+        allProperties.add(new Property("Bronx Zoo", 400, 30));
+        allProperties.add(new Property("Coney Island", 400, 30));
+        allProperties.add(new Property("Bryant Park Library", 400, 30));
+        allProperties.add(new Property("Coney Island Aquarium", 300, 20));
+        allProperties.add(new Property("Rockefeller Plaza", 300, 20));
+        allProperties.add(new Property("AMC Times Square", 300, 20));
+        allProperties.add(new Property("La Bagel", 250, 15));
+        allProperties.add(new Property("Dave & Busters", 200, 10));
+        allProperties.add(new Property("The Strand Bookstore", 200, 10));
+        allProperties.add(new Property("Petco", 200, 10));
+        allProperties.add(new Property("Rocky's", 100, 5));
+        allProperties.add(new Property("Pizza Parlor", 100, 5));
+        allProperties.add(new Property("Toys-R-Us", 100, 5));
+
+
     }
 
 
