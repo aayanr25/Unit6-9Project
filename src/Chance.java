96a21a8c60c4ac7a412ab60ae0c66fd72cba2d4e
BT_1N3_05 <me@me>
2024-04-03 09:26:40 -0400

    more chance stuff

--- src/Chance.java
+++ src/Chance.java
@@ -12,7 +12,32 @@ public class Chance extends Property {
 
     private void moveToGO() {
         System.out.println("Advance to GO & Collect $200");
-        player.setX(0);
         player.setY(0);
+        player.setX(0);
+    }
+
+    private void getOutOfJailFree() {
+
+    }
+
+    private void goForward() {
+        System.out.println("Move forward 3 spaces");
+        player.move(3);
+    }
+
+    private void tax() {
+        int num = (int) (Math.random() * 10 + 1);
+        int x = 0;
+        if (num <= 5) {
+            x = 25;
+        } else if (num <= 7) {
+            x = 50;
+        } else {
+            x = 100;
+        }
+
+        System.out.println("Pay tax of $" + x);
+
+        player.deductMoney(x);
     }
 }
